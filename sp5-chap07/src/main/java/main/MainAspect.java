1510ba17b1f8ba01ea01d26ff85028b59b087e86
Kosh <kosh7707@naver.com>
2023-01-08 20:32:33 +0900

    약 2달만에 다시하는 행복한 스프링

---
+++ sp5-chap07/src/main/java/main/MainAspect.java
@@ -0,0 +1,17 @@
+package main;
+
+import chap07.RecCalculator;
+import config.AppCtx;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainAspect {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
+
+        RecCalculator cal = ctx.getBean("calculator", RecCalculator.class);
+        long five = cal.factorial(5);
+        System.out.println("cal.factorial(5) = " + five);
+        System.out.println(cal.getClass().getName());
+        ctx.close();
+    }
+}
